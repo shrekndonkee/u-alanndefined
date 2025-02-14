dfdd7bb2a69c00c24808c24653394a488093ab7a
Alan Martinez-Diaz <alanmartindiaz21@gmail.com>
2025-02-13 21:54:04 -0700

    implented function types

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
+        main: resolve(root, 'index.html'),
+                basicTypes: resolve(root, 'basicTypes', 'index.html'),
+                functionTypes: resolve(root, 'functionTypes', 'index.html'),
         //nested: resolve(root, 'nested/index.html),
 
       }
