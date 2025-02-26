6320e143e1c0257a8d448be37d3043b71448a92d
Alan Martinez-Diaz <alanmartindiaz21@gmail.com>
2025-02-25 23:02:25 -0700

    fixed a coding error

--- src/utils/exampleLoadFuncs.ts
+++ src/utils/exampleLoadFuncs.ts
@@ -1,6 +1,6 @@
 import hljs from "highlight.js/lib/core"
 import typescript from "highlight.js/lib/languages/typescript"
-
+import type { CodeExample } from '../types/CodeExample'
 // Then register the languages you need
 hljs.registerLanguage("typescript", typescript)
 
